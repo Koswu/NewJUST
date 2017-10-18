d92dc1300cfb1f10489c7c82cfdbe9afd5fc9d97
yueyong1030@outlook.com <yueyong1030@outlook.com>
2017-10-18 15:32:22 +0800

    新增了设置界面，添加了个人资料这一部分

--- app/src/main/java/com/example/lyy/newjust/SettingsActivity.java
+++ app/src/main/java/com/example/lyy/newjust/SettingsActivity.java
@@ -1,13 +1,57 @@
 package com.example.lyy.newjust;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        init();
+
+    }
+
+    private void init() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.setting_toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
+        TextView tv_profile = (TextView) findViewById(R.id.tv_profile);
+        tv_profile.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_profile:
+                Intent intent = new Intent(SettingsActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
